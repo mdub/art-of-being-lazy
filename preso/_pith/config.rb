9dc5e82170a74867e3be3330e8c31faf1cbf0ad1
Mike Williams <mdub@dogbiscuit.org>
2011-06-29 14:27:24 +1000

    Don't need coderay.

--- preso/_pith/config.rb
+++ preso/_pith/config.rb
@@ -1,12 +1,5 @@
-require 'coderay'
-
 project.helpers do
 
-  def code(content)
-    tokens = CodeRay.scan(content, :ruby)
-    tokens.div :line_numbers => :inline, :css => :class
-  end
-  
   def raw_link(url)
     %{<a href="#{url}">#{url}</a>}
   end
