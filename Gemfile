2a96a243e5dc5c6b65f6bf66876eb7fbd8a82628
Mike Williams <mdub@dogbiscuit.org>
2016-05-12 18:10:53 +1000

    Add "lazily".

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source "https://rubygems.org"
 
 gem "enumerating"
+gem "lazily"
 
 gem "rake"
 gem "rspec"
--- lib/laziness.rb
+++ lib/laziness.rb
@@ -1,3 +1,4 @@
+require 'lazily'
 require 'laziness/selector'
 require 'laziness/some_numbers'
 require 'laziness/fibonacci_sequence'
